b7244c75ec69a2b56c9996929c4ff1c845d97a56
Steve Goodrich <steven.goodrich@landesk.com>
2018-10-31 16:41:59 -0600

    Updated fluent-sqlite

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     dependencies: [
         // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
-        .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0-rc"),
+        .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0"),
 
         // 🍃 An expressive, performant, and extensible templating language built for Swift.
         .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0"),
